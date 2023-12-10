ce150dc3e13c41bab425e2eca76a4fe0d17b9dfc
wagone123 wagone <remigarcia181805@gmail.com>
2023-12-10 02:17:43 +0000

    v1.4

--- add-fetch/ipadd.py
+++ add-fetch/ipadd.py
@@ -1,45 +1,66 @@
 import requests
 from concurrent.futures import ThreadPoolExecutor
 from urllib.parse import urlencode
+import time
 
 API_ENDPOINT = "https://api.proxyscrape.com/v2/account/datacenter_shared/whitelist"
+MAX_RETRIES = 3
 
-def send_request(api_key, action, ip_to_set, proxy):
-    params = {
-        'auth': api_key,
-        'type': 'remove' if action.lower() == 'delete' else 'add',
-        'ip[]': ip_to_set
-    }
+def send_request(api_key, action, ip_to_set, proxy=None, retries=0):
+    try:
+        params = {
+            'auth': api_key,
+            'type': 'remove' if action.lower() == 'delete' else 'add',
+            'ip[]': ip_to_set
+        }
 
-    # Use the 'proxies' parameter to specify the proxy for this request
-    response = requests.get(API_ENDPOINT, params=urlencode(params), proxies={'http': proxy, 'https': proxy})
-    print(f"Response for {api_key} ({action}) using proxy {proxy}: {response.text}")
+        proxies = {'http': proxy, 'https': proxy} if proxy else None
+        response = requests.get(API_ENDPOINT, params=urlencode(params), proxies=proxies)
+        response.raise_for_status()
+
+        print(f"Response for {api_key} ({action}) using proxy {proxy}: {response.text}")
+
+    except requests.RequestException as e:
+        print(f"Error for {api_key} ({action}) using proxy {proxy}: {e}")
+
+        if retries < MAX_RETRIES:
+            print(f"Retrying {retries + 1}...")
+            time.sleep(1)
+            send_request(api_key, action, ip_to_set, proxy, retries + 1)
+        else:
+            print("Max retries reached. Skipping.")
+
+def delete_ip(api_keys, ip_to_delete, proxies):
+    for api_key, proxy in zip(api_keys, proxies):
+        send_request(api_key, 'delete', ip_to_delete, proxy)
+
+def add_ip(api_keys, ip_to_add, proxies=None):
+    with ThreadPoolExecutor(max_workers=15) as executor:  # Set max_workers to 15
+        tasks = [(api_key, 'add', ip_to_add, proxy) for api_key, proxy in zip(api_keys, proxies)]
+        futures = [executor.submit(send_request, *task) for task in tasks]
+
+        # Wait for all the futures to complete
+        for future in futures:
+            future.result()
 
 def main():
     with open('api.txt', 'r') as api_file:
         api_keys = [line.strip() for line in api_file if line.strip()]
-
-    with open('proxy.txt', 'r') as proxy_file:
-        proxies = [line.strip() for line in proxy_file if line.strip()]
 
     action = input("Enter 'add' or 'delete': ").lower()
     if action not in ('add', 'delete'):
         print("Invalid action. Exiting.")
         return
 
+    ip_to_set = input("Enter IP: ")
+
+    with open('proxy.txt', 'r') as proxy_file:
+        proxies = [line.strip() for line in proxy_file if line.strip()]
+
     if action == 'delete':
-        ip_to_set = input("Enter IP to delete: ")
+        delete_ip(api_keys, ip_to_set, proxies)
     else:
-        ip_to_set = input("Enter IP to add: ")
-
-    with ThreadPoolExecutor(max_workers=len(api_keys)) as executor:
-        # Pair each API key with a different proxy
-        tasks = [(api_key, action, ip_to_set, proxy) for api_key, proxy in zip(api_keys, proxies)]
-        futures = [executor.submit(send_request, *task) for task in tasks]
-
-        # Wait for all the futures to complete
-        for future in futures:
-            future.result()
+        add_ip(api_keys, ip_to_set, proxies)
 
 if __name__ == "__main__":
     main()
--- add-fetch/myip.py
+++ add-fetch/myip.py
@@ -1,18 +1,61 @@
 import requests
+from concurrent.futures import ThreadPoolExecutor
+from urllib.parse import urlencode
 
-def get_ip_info():
-    api_url = "https://api.myip.com"
+API_ENDPOINT = "https://api.proxyscrape.com/v2/account/datacenter_shared/whitelist"
 
+def send_request(api_key, action, ip_to_set, proxy=None):
     try:
-        response = requests.get(api_url)
-        response.raise_for_status()  # Raise an HTTPError for bad responses
+        params = {
+            'auth': api_key,
+            'type': 'remove' if action.lower() == 'delete' else 'add',
+            'ip[]': ip_to_set
+        }
 
-        ip_info = response.json()
-        print("IP Information:")
-        print(f"IP Address: {ip_info['ip']}")
-        print(f"Location: {ip_info.get('location', {}).get('city', 'N/A')}, {ip_info.get('location', {}).get('region', 'N/A')}, {ip_info.get('location', {}).get('country', 'N/A')}")
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
+        # Use the 'proxies' parameter to specify the proxy for this request if provided
+        proxies = {'http': proxy, 'https': proxy} if proxy else None
+        response = requests.get(API_ENDPOINT, params=urlencode(params), proxies=proxies)
+        response.raise_for_status()  # Raise HTTPError for bad responses
+
+        print(f"Response for {api_key} ({action}) using proxy {proxy}: {response.text}")
+
+    except requests.RequestException as e:
+        print(f"Error for {api_key} ({action}) using proxy {proxy}: {e}")
+
+def delete_ip(api_key, ip_to_delete):
+    send_request(api_key, 'delete', ip_to_delete)
+
+def add_ip(api_key, ip_to_add, proxies=None):
+    with ThreadPoolExecutor() as executor:
+        # Use concurrent connections for IP addition
+        tasks = [(api_key, 'add', ip_to_add, proxy) for proxy in proxies]
+        futures = [executor.submit(send_request, *task) for task in tasks]
+
+        # Wait for all the futures to complete
+        for future in futures:
+            future.result()
+
+def main():
+    with open('api.txt', 'r') as api_file:
+        api_keys = [line.strip() for line in api_file if line.strip()]
+
+    action = input("Enter 'add' or 'delete': ").lower()
+    if action not in ('add', 'delete'):
+        print("Invalid action. Exiting.")
+        return
+
+    ip_to_set = input("Enter IP: ")
+
+    if action == 'delete':
+        # Perform IP deletion one by one
+        for api_key in api_keys:
+            delete_ip(api_key, ip_to_set)
+    else:
+        # Read proxies from proxy.txt for concurrent IP addition
+        with open('proxy.txt', 'r') as proxy_file:
+            proxies = [line.strip() for line in proxy_file if line.strip()]
+
+        add_ip(api_keys[0], ip_to_set, proxies)
 
 if __name__ == "__main__":
-    get_ip_info()
+    main()
